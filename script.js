a92e85c3b6f85a4ea97a74f124e4bedbc8961774
jjgome <a.singh263@share.epsb.ca>
2024-11-26 15:21:44 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// Area of trapezoid
+document.getElementById("calc").addEventListener("click", areaTrap);
+
+function areaTrap() {
+  // Input
+  var bValue = Number(document.getElementById("b").value);
+  var cValue = Number(document.getElementById("c").value);
+
+  // Output
+
+  document.getElementById("a").value = " ";
+  document.getElementById("b").value = " ";
+  document.getElementById("c").value = " ";
+}
+
+// https://github.com/FDDojoMaster/quadraticEquationCalculator/blob/master/index.html
+
+// https://www.youtube.com/watch?v=0ELC8R69Rkw
